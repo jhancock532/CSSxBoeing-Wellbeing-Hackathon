aa054955228339d12278369f37de83de303f7c48
James Hancock <jhancock532@gmail.com>
2019-03-08 15:21:30 +0000

    Completed the config classes

--- src/main/java/packages/HackathonApplication.java
+++ src/main/java/packages/HackathonApplication.java
@@ -1,4 +1,4 @@
-package com.manyways.hackathon;
+package packages;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/packages/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+
+package packages.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService(){
+        //Administration sign-on service.
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("jh17112@my.bristol.ac.uk")
+                .password("password")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/","/legal").permitAll()
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+            .logout()
+                .permitAll();
+    }
+}

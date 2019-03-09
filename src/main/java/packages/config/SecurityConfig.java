9e9aa18722ef825079dfda0572a20a0b213d33dc
James Hancock <jhancock532@gmail.com>
2019-03-09 14:20:05 +0000

    Final tweaks for the presentation

--- src/main/java/packages/config/SecurityConfig.java
+++ src/main/java/packages/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/","/search","principles","site-information").permitAll()
+                .antMatchers("/","/search","/principles","/site-information", "/js/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
             .formLogin()
